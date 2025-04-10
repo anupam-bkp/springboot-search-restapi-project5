2ec9771546864d336407c88e374af70c579bc48d
anupam <anupam.nitdgp14@gmail.com>
2025-04-10 21:56:24 +0530

    Initialize the projects with required dependencies and initial code.

---
+++ src/main/java/com/learner/search/SpringbootSearchRestapiProject5Application.java
@@ -0,0 +1,13 @@
+package com.learner.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSearchRestapiProject5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSearchRestapiProject5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/learner/search/SpringbootSearchRestapiProject5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learner.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSearchRestapiProject5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
